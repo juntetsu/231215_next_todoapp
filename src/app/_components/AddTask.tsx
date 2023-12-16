c777d1a510112a1cc9b7f7b8d794ec5bdd60f3c0
jun <juntetsu.1012@gmail.com>
2023-12-17 04:09:31 +0900

    タスクを追加するAPI

--- src/app/_components/AddTask.tsx
+++ src/app/_components/AddTask.tsx
@@ -1,17 +1,36 @@
-import React from 'react'
+'use client'
+
+import { addTodo } from "@/app/_lib/api";
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import {v4 as uuidv4} from 'uuid'
 
 const AddTask = () => {
+  const [todoTitle, setTodoTitle] = useState("");
+
+  const handleSubmit = async (e: FormEvent) => {
+    // Prevent the default behaviour of the form
+    e.preventDefault;
+    // Add the todo
+    await addTodo({ id: uuidv4(), text: todoTitle });
+    // Reset the input field
+    setTodoTitle("");
+  };
+
   return (
     <form className="mb-4 space-y-3">
       <input
         type="text"
         className="w-full border px-4 py-2 rounded-lg focus:outline-none focus:border-blue-400"
+        onChange={(e: ChangeEvent<HTMLInputElement>) => setTodoTitle(e.target.value)}
       />
-      <button className="w-full px-4 py-2 text-white bg-blue-500 rounded transform transition-transform duration-200 hover:bg-blue-400 hover:scale-95">
+      <button
+        className="w-full px-4 py-2 text-white bg-blue-500 rounded transform transition-transform duration-200 hover:bg-blue-400 hover:scale-95"
+        onClick={handleSubmit}
+      >
         Add Task
       </button>
     </form>
   );
-}
+};
 
-export default AddTask
+export default AddTask;
--- src/app/_lib/api.ts
+++ src/app/_lib/api.ts
@@ -1,8 +1,26 @@
 import { TodoType } from "./types";
 
+// Todoを取得する関数
 export const getAllTodos = async (): Promise<TodoType[]> => {
-  const res = await fetch("http://localhost:3001/tasks", {cache: 'no-store'});
+  const res = await fetch("http://localhost:3001/tasks", { cache: "no-store" });
   const todos = res.json();
 
   return todos;
+};
+
+// Todoを追加する関数
+export const addTodo = async (todo: TodoType): Promise<TodoType> => {
+  const res = await fetch("http://localhost:3001/tasks", {
+    // POSTメソッドで送信する
+    method: "POST",
+    headers: {
+      //JSON 形式のデータを送るので、Content-Type に application/json を指定
+      "Content-Type": "application/json",
+    },
+    //送信する JSON データをシリアライズ（JSON 文字列に変換）してリクエストボディに指定
+    body: JSON.stringify(todo),
+  });
+  const newTodo = res.json();
+
+  return newTodo;
 };
